8639f73353894f40c05a417dbaafbcbda2c0a309
Darren Tseng <darren2sk8@gmail.com>
2017-04-03 15:59:12 +0100

    better error handling

--- manage.py
+++ manage.py
@@ -2,41 +2,41 @@
 """Run bexplorer application. Similar to manage.py"""
 from bexplorer.bexplorer import *
 from flask_script import Command, Manager, Option, Server
-from gevent import pywsgi
+# from gevent import pywsgi
 
 manager = Manager(app)
 # DEBUG FALSE BEFORE DEPLOYING!
-DEBUG = False
+DEBUG = True
 KEYPATH = None
 CERTPATH = None
+PORT = 5000
 
 
 class Server(Command):
     """Initializes server"""
 
-    def __init__(self, secure=False):
-        self.secure = secure
+    # def __init__(self, secure=False):
+    # self.secure = secure
 
-    def get_options(self):
-        return [
-            Option('-s', '--secure', dest='secure',
-                   default=self.secure)
-        ]
+    # def get_options(self):
+    #     return [
+    #         Option('-s', '--secure', dest='secure',
+    #                default=self.secure)
+    #     ]
 
     def run(self):
-        secure = secure.lower() == "true"
-        if secure:
-            print("Running websocket server in secure ssl mode.")
-            server = pywsgi.WSGIServer(
-                ('', 5001), app, keyfile=KEYPATH, certfile=CERTPATH)
-        else:
-            print("Running websocket server.")
-            server = pywsgi.WSGIServer(('', 5001), app)
+        print("Running server.")
+        server = app.run(host='0.0.0.0', port=PORT, debug=DEBUG)
+        # secure = secure.lower() == "true"
+        # if secure:
+        #     print("Running websocket server in secure ssl mode.")
+        #     server = pywsgi.WSGIServer(
+        #         ('', 5001), app, keyfile=KEYPATH, certfile=CERTPATH)
+        # else:
+        #     print("Running websocket server.")
+        #     server = pywsgi.WSGIServer(('', 5001), app)
 
-        server.serve_forever()
-
-        # print("Running server.")
-        # server = app.run(host='0.0.0.0', port=5000, debug=DEBUG)
+        # server.serve_forever()
 
 
 manager.add_command('server', Server())
