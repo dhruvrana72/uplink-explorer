5b0248c52f60111e9fc9a9df4b7d6e2a26203dc0
Anthony <faineance@users.noreply.github.com>
2017-10-26 11:08:01 +0100

    Eek sentry keys (#62)
    * transfers
    
    * pep8
    
    * initial call contract
    
    * better, actually calls
    
    * fixed calling
    
    * handle booleanfield form
    
    * cleanup
    
    * add state
    
    * message if no methods availiable
    
    * ooh noo

--- uplink_explorer/extensions.py
+++ uplink_explorer/extensions.py
@@ -3,7 +3,7 @@
 import os
 from uplink.session import UplinkSession
 from config import config, ProdConfig
-
+import os
 rpc_addr = config.RPC_HOST
 
 print("RPC listening on " + rpc_addr)
@@ -11,8 +11,8 @@ print("RPC listening on " + rpc_addr)
 # register extensions
 uplink = UplinkSession(addr=rpc_addr)
 
-if config == ProdConfig:
+if config == ProdConfig and os.environ.get['SENTRY']:
     from raven.contrib.flask import Sentry
 
     # XXX -- remove from open sourcing and put as env var
-    sentry = Sentry(dsn='https://8590448340a54eb2a9c47b3628860560:23f589dd7412424ebee127f2a0114dd7@sentry.io/205685')
+    sentry = Sentry(dsn=os.environ['SENTRY'])
